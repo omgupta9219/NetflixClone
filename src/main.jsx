b4c0c1abdb12f2dc021a07eacc7da128a1170b21
omgupta9219 <omgupta9219@gmail.com>
2025-11-23 22:52:42 +0530

    Updated the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React ,{ StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-
-  </React.StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
